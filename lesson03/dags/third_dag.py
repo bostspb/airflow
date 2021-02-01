416977cc3e7345527ce2839b146869cc15f719b5
Zakhar <z.chernyak@filanco.ru>
2021-02-01 10:02:18 +0300

    Урок 3. Разработка потоков данных

---
+++ lesson03/dags/third_dag.py
@@ -0,0 +1,117 @@
+"""
+ssh root@79.143.30.194
+u3kn1zbaw9
+
+airflow webserver -p 8181
+airflow scheduler
+
+http://79.143.30.194:8181/home
+
+postgres
+Pe4g5k$kkrz6
+
+airflow
+airflow
+
+postgres_default
+
+sudo -u postgres psql
+CREATE database titanic;
+GRANT all privileges on database titanic to airflow;
+\q
+
+
+названия таблиц
+
+
+
+1. Все функции вынесены в отдельный модуль и в DAG файле только сама структура графа (директория модулей должна быть в PATH)
+2. Отказ от работы с локальными файлами:
+    - сначала скачанный датасет пушится в XCom (он весит ~50 КБ)
+    - затем он пуллится из XCom и передается двум преобразованиям (pivot и mean_fare)
+3. Результаты преобразований записываются в две таблицы локальной базы PostgreSQL (Connections+Hooks или psycopg2/sqlalchemy).
+4. Имена таблиц в PostgreSQL заданы в Variables
+"""
+
+import os
+import sys
+import datetime as dt
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, path)
+
+from modules.utils import get_titanic_dataset, pivot_dataset, mean_fare_per_class
+import modules.config as cfg
+
+
+# базовые аргументы DAG
+args = {
+    'owner': 'airflow',  # Информация о владельце DAG
+    'start_date': dt.datetime(2020, 12, 23),  # Время начала выполнения пайплайна
+    'retries': 1,  # Количество повторений в случае неудач
+    'retry_delay': dt.timedelta(minutes=1),  # Пауза между повторами
+    'depends_on_past': False,  # Запуск DAG зависит ли от успешности окончания предыдущего запуска по расписанию
+}
+
+# В контексте DAG'а зададим набор task'ок
+with DAG(
+        dag_id='my_super_titanic',  # Имя DAG
+        schedule_interval=None,  # Периодичность запуска, например, "00 15 * * *"
+        default_args=args,  # Базовые аргументы
+) as dag:
+
+    first_task = BashOperator(
+        task_id='first_task',
+        bash_command='echo "Here we start! Info: run_id={{ run_id }} | dag_run={{ dag_run }}"',
+        dag=dag,
+    )
+
+    get_dataset = PythonOperator(
+        task_id='get_titanic_dataset',
+        python_callable=get_titanic_dataset,
+        dag=dag,
+    )
+
+    create_pg_tables = PostgresOperator(
+        task_id='create_table',
+        sql='''
+            CREATE TABLE IF NOT EXISTS titanic.pivot(                
+                sex VARCHAR (8) NOT NULL,
+                class01 integer NOT NULL, 
+                class02 integer NOT NULL, 
+                class03 integer NOT NULL
+            );
+            CREATE TABLE IF NOT EXISTS titanic.mean_fare_per_class(
+                pclass integer NOT NULL, 
+                fare numeric NOT NULL
+            );        
+        ''',
+        dag=dag,
+    )
+
+    mean_fares_titanic_dataset = PythonOperator(
+        task_id='mean_fares_titanic_dataset',
+        provide_context = True,
+        python_callable=mean_fare_per_class,
+        dag=dag,
+    )
+
+    pivot_titanic_dataset = PythonOperator(
+        task_id='pivot_dataset',
+        provide_context=True,
+        python_callable=pivot_dataset,
+        dag=dag,
+    )
+
+    last_task = BashOperator(
+        task_id='last_task',
+        bash_command='echo "Pipeline finished! Execution date is {{ ds }}"',
+        dag=dag,
+    )
+
+
+    first_task >> get_dataset >> create_pg_tables >> [mean_fares_titanic_dataset, pivot_titanic_dataset] >> last_task
---
+++ lesson03/modules/utils.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from airflow.hooks.postgres_hook import PostgresHook
+
+
+pg_hook = PostgresHook(postgres_conn_id='postgres_default')
+
+
+def get_titanic_dataset() -> dict:
+    url = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
+    df = pd.read_csv(url)
+    return df.to_json()
+
+
+def pivot_dataset(**kwargs):
+    ti = kwargs['ti']
+    titanic_json = ti.xcom_pull(key=None, task_ids='get_titanic_dataset')
+    titanic_df = pd.read_json(titanic_json)
+    result = titanic_df.pivot_table(
+        index=['Sex'],
+        columns=['Pclass'],
+        values='Name',
+        aggfunc='count'
+    ).reset_index().to_dict(orient='records')
+    pg_insert = 'insert into titanic.pivot (sex, class01, class02, class03) values (%s, %s, %s, %s)'
+    pg_hook.run(pg_insert, parameters=(result[0]['Sex'], result[0][1], result[0][2], result[0][3],))
+    pg_hook.run(pg_insert, parameters=(result[1]['Sex'], result[1][1], result[1][2], result[1][3],))
+
+
+def mean_fare_per_class(**kwargs):
+    ti = kwargs['ti']
+    titanic_json = ti.xcom_pull(key=None, task_ids='get_titanic_dataset')
+    titanic_df = pd.read_json(titanic_json)
+    result = titanic_df.groupby('Pclass')['Fare'].mean().reset_index().to_dict(orient='records')
+    pg_insert = 'insert into titanic.mean_fare_per_class (pclass, fare) values (%s, %s)'
+    pg_hook.run(pg_insert, parameters=(result[0]['Pclass'], result[0]['Fare'],))
+    pg_hook.run(pg_insert, parameters=(result[1]['Pclass'], result[1]['Fare'],))
+    pg_hook.run(pg_insert, parameters=(result[2]['Pclass'], result[2]['Fare'],))
